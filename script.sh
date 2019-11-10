5d8cfafb2cfbe89aa277fed24d3bc487039d4031
lawn58 <47656823+lawn58@users.noreply.github.com>
2019-11-10 21:11:49 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #create raid 5 :)
-sudo mdamd --zero-superblock --force /dev/sd{b,c,d,e,f} &&
-sudomdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f} &&
+sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f} &&
+sudo mdadm --create --verbose /dev/md0 -l 5 -n 5 /dev/sd{b,c,d,e,f} &&
 # "-l" -- raid level ; "-n" -- quantity of disks
 sudo mkdir -p /etc/mdadm && sudo touch /etc/mdadm/mdadm.conf &&
 sudo mdadm --detail --scan --verbose|awk /ARRAY/{print} >> /etc/mdadm/mdadm.conf && 
